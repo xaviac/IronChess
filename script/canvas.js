44f2009f7d6625a784068dd495bbf3d647f2e89d
Javier Acevedo <jacevedomx@gmail.com>
2020-08-18 16:52:49 -0500

    Tablero

---
+++ script/canvas.js
@@ -0,0 +1,38 @@
+function board() {
+    // Apuntamos al canvas
+      const $canvas = document.getElementById("canvas");
+      const ctx = $canvas.getContext("2d");
+    // Creamos el ctx
+    // Dibujamos el borde
+      ctx.strokeRect(10, 10, 820, 820);
+      ctx.strokeRect(20, 20, 800, 800);
+    // Dibujamos dentro del primer boder
+      let c = 20;
+      // Creamos un blucle for
+      for (i = 1; i < 5; i++) {
+        ctx.clearRect(20, c, 100, 100);
+        ctx.fillRect(120, c, 100, 100);
+        ctx.clearRect(220, c, 100, 100);
+        ctx.fillRect(320, c, 100, 100);
+        ctx.clearRect(420, c, 100, 100);
+        ctx.fillRect(520, c, 100, 100);
+        ctx.clearRect(620, c, 100, 100);
+        ctx.fillRect(720, c, 100, 100);
+    
+        c = c + 100;
+    
+        ctx.fillRect(20, c, 100, 100);
+        ctx.clearRect(120, c, 100, 100);
+        ctx.fillRect(220, c, 100, 100);
+        ctx.clearRect(320, c, 100, 100);
+        ctx.fillRect(420, c, 100, 100);
+        ctx.clearRect(520, c, 100, 100);
+        ctx.fillRect(620, c, 100, 100);
+        ctx.clearRect(720, c, 100, 100);
+    
+        c = c + 100;
+      }
+    }
+    
+    window.addEventListener("load", board, false);
+    

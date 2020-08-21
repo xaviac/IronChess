5614ed5bb5679362686bf759efabf75310127aa3
Javier Acevedo <jacevedomx@gmail.com>
2020-08-20 22:34:41 -0500

    Sonido y controles de audio

--- script/canvas.js
+++ script/canvas.js
@@ -223,11 +223,17 @@ const player = {
 document.addEventListener("keydown", (event) => {
   if (event.keyCode === 37) {
     playerMove(-1);
+  } else if (event.keyCode === 65) {
+    playerMove(-1);
   } else if (event.keyCode === 39) {
+    playerMove(1);
+  } else if (event.keyCode === 68) {
     playerMove(1);
   } else if (event.keyCode === 40) {
     playerDrop();
-  } else if (event.keyCode === 81) {
+  } else if (event.keyCode === 83) {
+    playerDrop();
+  } else if (event.keyCode === 38) {
       playerRotate(-1);
   } else if (event.keyCode === 87) {
       playerRotate(1);
